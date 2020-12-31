a05f4c8bbd8baf9838aafdee179f10294a1be628
Jean-Paul van Ravensberg <14926452+DevSecNinja@users.noreply.github.com>
2020-12-31 15:02:33 +0000

    Bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     name="aiobittrexapi",
     packages=find_packages(include=["aiobittrexapi"]),
     url="https://github.com/DevSecNinja/aiobittrexapi",
-    version="0.1.2",
+    version="1.0.0",
     zip_safe=False,
 )
